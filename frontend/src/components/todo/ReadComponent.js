bc08073fcbb05ffd41723d007ffde96f71a4615e
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2024-03-24 21:04:35 +0900

    useCustomMove hook 만들기 및 적용

--- frontend/src/components/todo/ReadComponent.js
+++ frontend/src/components/todo/ReadComponent.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getOne } from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
 
 const initState = {
 	tno: 0,
@@ -11,10 +12,10 @@ const initState = {
 
 const ReadComponent = ({ tno }) => {
 	const [todo, setTodo] = useState(initState);
+	const { moveToList } = useCustomMove();
 
 	useEffect(() => {
 		getOne(tno).then((data) => {
-			console.log(data);
 			setTodo(data);
 		});
 	}, [tno]);
@@ -26,6 +27,16 @@ const ReadComponent = ({ tno }) => {
 			{makeDiv("Title", todo.title)}
 			{makeDiv("Due Date", todo.dueDate)}
 			{makeDiv("Complete", todo.complete ? "Completed" : "Not Yet")}
+
+			<div className="flex justify-end p-4">
+				<button
+					type="button"
+					className="rounded p-4 m-2 text-xl text-white bg-blue-500"
+					onClick={() => moveToList()}
+				>
+					List
+				</button>
+			</div>
 		</div>
 	);
 };
